0510a882fd4d737da7ea305de522389818189936
parhamk75 <36754636+parhamk75@users.noreply.github.com>
2019-06-19 13:07:06 +0430

    NRF_IS Function Declarations done + NRF_Typedef added

--- NRF24L01-Instruction-Set/NRF24L01_IS.c
+++ NRF24L01-Instruction-Set/NRF24L01_IS.c
@@ -1,13 +1,13 @@
 #include "NRF24L01_IS.h"
 
 
-HAL_StatusTypeDef NRF_INS_Read_Reg(SPI_HandleTypeDef* hspi, uint8_t adr, uint8_t d_len, uint8_t* pdata, uint8_t* STAT_Reg){
+HAL_StatusTypeDef NRF_INS_Read_Reg(NRF24L01_t* nrf, uint8_t adr, uint8_t d_len, uint8_t* pdata, uint8_t* STAT_Reg){
 
 	if( adr >= 32 )
 		return HAL_ERROR;
 	
-	HAL_SPI_TransmitReceive( hspi, &adr, STAT_Reg, 1, HAL_MAX_DELAY);
-	HAL_SPI_Receive( hspi, pdata, d_len, HAL_MAX_DELAY);
+	HAL_SPI_TransmitReceive( nrf->hspi, &adr, STAT_Reg, 1, HAL_MAX_DELAY);
+	HAL_SPI_Receive( nrf->hspi, pdata, d_len, HAL_MAX_DELAY);
 	
 	return HAL_OK;
 }
--- NRF24L01-Instruction-Set/NRF24L01_IS.h
+++ NRF24L01-Instruction-Set/NRF24L01_IS.h
@@ -1,6 +1,51 @@
 #include "main.h"
 #include "stm32f4xx_hal.h"
 
-HAL_StatusTypeDef NRF_INS_Read_Reg(SPI_HandleTypeDef* hspi, uint8_t adr, uint8_t d_len, uint8_t* pdata, uint8_t* STAT_Reg );
+typedef enum 
+{
+  RX_MODE      				= 0x00U,
+  TX_MODE    					= 0x01U,
+  STANDBY_I_MODE      = 0x02U,
+  STANDBY_II_MODE			= 0x03U,
+	POWER_DOWN_MODE 		= 0x04U
+} NRF_ModeTypeDef;
 
+typedef struct{
+	SPI_HandleTypeDef*  hspi;
+	NRF_ModeTypeDef 		mode;
+	
+}NRF24L01_t;
 
+HAL_StatusTypeDef NRF_INS_Read_Reg(			NRF24L01_t* nrf,
+																				uint8_t adr, 
+																				uint8_t d_len, 
+																				uint8_t* pdata, 
+																				uint8_t* STAT_Reg );
+
+HAL_StatusTypeDef NRF_INS_Write_Reg(		NRF24L01_t* nrf,
+																				uint8_t adr, 
+																				uint8_t d_len, 
+																				uint8_t* pdata, 
+																				uint8_t* STAT_Reg );
+
+HAL_StatusTypeDef NRF_INS_Read_Rx_PL(		NRF24L01_t* nrf, 
+																				uint8_t d_len, 
+																				uint8_t* pdata, 
+																				uint8_t* STAT_Reg );
+
+HAL_StatusTypeDef NRF_INS_Write_Tx_PL(	NRF24L01_t* nrf, 
+																				uint8_t d_len, 
+																				uint8_t* pdata, 
+																				uint8_t* STAT_Reg );
+
+HAL_StatusTypeDef NRF_INS_Flush_Tx(			NRF24L01_t* nrf,
+																				uint8_t* STAT_Reg );
+
+HAL_StatusTypeDef NRF_INS_Flush_Rx(			NRF24L01_t* nrf,
+																				uint8_t* STAT_Reg );
+
+HAL_StatusTypeDef NRF_INS_Reuse_TxPL(		NRF24L01_t* nrf,
+																				uint8_t* STAT_Reg );
+
+HAL_StatusTypeDef NRF_INS_NOP(					NRF24L01_t* nrf,
+																				uint8_t* STAT_Reg );

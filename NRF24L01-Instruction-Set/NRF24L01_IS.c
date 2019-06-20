fa1ae3b49dab88ceabf862169bce3230a574b81f
ElaheBv <elahe.bvakili97@gmail.com>
2019-06-20 17:01:52 +0430

    Function NRF_INS_Write_Reg definition completed in NRF_IS

--- NRF24L01-Instruction-Set/NRF24L01_IS.c
+++ NRF24L01-Instruction-Set/NRF24L01_IS.c
@@ -11,10 +11,30 @@ HAL_StatusTypeDef NRF_INS_Read_Reg   (NRF24L01_t* nrf,
 		return HAL_ERROR;
 	
 	HAL_SPI_TransmitReceive( nrf->hspi, &adr, STAT_Reg, 1, HAL_MAX_DELAY);
-	//HAL_Delay(1);
+//HAL_Delay(1);
 	HAL_SPI_Receive( nrf->hspi, pdata, d_len, HAL_MAX_DELAY);
 	
 	
 	return HAL_OK;
 }
+
+HAL_StatusTypeDef NRF_INS_Write_Reg(		NRF24L01_t* nrf,
+																				uint8_t adr, 
+																				uint8_t d_len, 
+																				uint8_t* pdata, 
+																				uint8_t* STAT_Reg ){				
+	 // Executable in power down and standby mode only
+		if(nrf->mode==0x02U || nrf->mode==0x03U	|| 	nrf->mode==0x04U)
+		{
+       if( adr >= 32 )
+		     return HAL_ERROR;		
+
+	      adr+=32;
+	      HAL_SPI_TransmitReceive( nrf->hspi, &adr, STAT_Reg, 1, HAL_MAX_DELAY);
+	    //HAL_Delay(1);
+	      HAL_SPI_Transmit( nrf->hspi, pdata, d_len, HAL_MAX_DELAY);
+    }
+																					
+	 return HAL_ERROR;												
+																			                   	}
 

094773e61b532bb21832ccfbd61a3ae1933bf5be
ElaheBv <elahe.bvakili97@gmail.com>
2019-06-20 17:25:52 +0430

    Function NRF_INS_Read_Rx_PL definition completed in NRF_IS

--- NRF24L01-Instruction-Set/NRF24L01_IS.c
+++ NRF24L01-Instruction-Set/NRF24L01_IS.c
@@ -24,7 +24,7 @@ HAL_StatusTypeDef NRF_INS_Write_Reg(		NRF24L01_t* nrf,
 																				uint8_t* pdata, 
 																				uint8_t* STAT_Reg ){				
 	 // Executable in power down and standby mode only
-		if(nrf->mode==0x02U || nrf->mode==0x03U	|| 	nrf->mode==0x04U)
+		if(nrf->mode==STANDBY_I_MODE || nrf->mode==STANDBY_II_MODE || 	nrf->mode==POWER_DOWN_MODE)
 		{
        if( adr >= 32 )
 		     return HAL_ERROR;		
@@ -34,7 +34,53 @@ HAL_StatusTypeDef NRF_INS_Write_Reg(		NRF24L01_t* nrf,
 	    //HAL_Delay(1);
 	      HAL_SPI_Transmit( nrf->hspi, pdata, d_len, HAL_MAX_DELAY);
     }
-																					
-	 return HAL_ERROR;												
+		
+   else		
+	    return HAL_ERROR;												
 																			                   	}
 
+HAL_StatusTypeDef NRF_INS_Read_Rx_PL(		NRF24L01_t* nrf, 
+																				uint8_t d_len, 
+																				uint8_t* pdata, 
+																				uint8_t* STAT_Reg ){
+			//Used in RX mode
+      if(nrf->mode != RX_MODE)
+				return HAL_ERROR;		
+																		
+			uint8_t Read_Rx_PL=97;
+				 HAL_SPI_TransmitReceive( nrf->hspi, &Read_Rx_PL, STAT_Reg, 1, HAL_MAX_DELAY);
+			 //HAL_Delay(1);
+	       HAL_SPI_Receive( nrf->hspi, pdata, d_len, HAL_MAX_DELAY);	
+			
+																	                         }
+
+																													
+																													
+																													
+																													
+																													
+																													
+																													
+																													
+																													
+																													
+																													
+																													
+																													
+																													
+																													
+																													
+																													
+																													
+																													
+																													
+																													
+																													
+																													
+																													
+																													
+																													
+																													
+																													
+																													
+																													
